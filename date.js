300e7f1a258b6e077a69fb3f159ee0352ed769d4
Khayyam Saleem <khayyam.saleem@gmail.com>
2016-06-27 02:50:52 -0400

    added changes from personal site

--- date.js
+++ date.js
@@ -1,15 +1,27 @@
 
 var ageCalc = function() {
-	var birthday = new Date(localStorage['dob']);
-	var today = new Date();
-	var duration = today - birthday;
-	var age = duration / 31556900000;
+//	var birthday = new Date(localStorage['dob']);
+//	var today = new Date();
+//	var duration = today - birthday;
+//	var age = duration / 31556900000;
+	var age = (new Date() - new Date(localStorage.dob)) / 31556900000;
 	$('#age').html('<h2>' + age.toFixed(15) + '</h2>');
-	console.log(age);
 };
 
 $(document).ready(function(){
-	if (typeof localStorage['dob'] != "undefined"){
+	if (((navigator.userAgent.toLowerCase().indexOf('webkit') > 0)  || 
+			 (navigator.userAgent.toLowerCase().indexOf('mobi')   > 0)) && 
+		   (typeof localStorage['dob'] == 'undefined') ){
+		$('#pikaday').remove();
+		$('footer').html("<input type='date' id='datepicker' name='age' /> <button id='button'>Motivate</button>");
+		$('#button').click(function(e){
+			e.preventDefault();
+			localStorage.dob = document.getElementById('datepicker').value;
+			$('form').fadeOut('fast');
+			$('body').append("<div id='age'></div>");
+			setInterval(ageCalc, 80);
+		});
+	} else if (typeof localStorage['dob'] != "undefined"){
 		$('form').remove();
 		$('body').append("<div id='age'></div>");
 		setInterval(ageCalc, 80);
@@ -21,5 +33,5 @@ $(document).ready(function(){
 			$('body').append("<div id='age'></div>");
 			setInterval(ageCalc, 80);
 		});
-	}
+	};
 });
